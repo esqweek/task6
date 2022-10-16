0f85f9f4d896ae7bbbf905a372059801fa89a2ba
esqweek <115408408+esqweek@users.noreply.github.com>
2022-10-16 17:09:19 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a, b, n;
+	cin >> n;
+	a = n / 1000;
+	b = n % 1000;
+	int s1, s2;
+	s1 = a % 10 + (a / 10) % 10 + a / 100;
+	s2 = b % 10 + (b / 10) % 10 + b / 100;
+	if (s1 == s2) cout << "YES";
+	else cout << "NO";
+}
+	
